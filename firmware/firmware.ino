d0a311a49087cf4177eed08f2350eaefc22f94ab
Felix Weichselgartner <felix.weichselgartner@t-online.de>
2021-08-03 22:50:47 +0200

    wemos d1 v2 version

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,28 +1,13 @@
 #include <Bitcoin.h>
+#include <LiquidCrystal.h>
 #include <stdint.h>
 
-/*
-#include <LiquidCrystal.h>
-
-const int PIN_RS = 7, PIN_E = 6, PIN_D4 = 5, PIN_D5 = 4, PIN_D6 = 3, PIN_D7 = 2;
+// For testbench 1:
+int button1 = D1;
+int button2 = D0;
+int led_board = LED_BUILTIN;
+const int PIN_RS = D3, PIN_E = D2, PIN_D4 = D5, PIN_D5 = D6, PIN_D6 = D7, PIN_D7 = D8;
 LiquidCrystal lcd(PIN_RS, PIN_E, PIN_D4, PIN_D5, PIN_D6, PIN_D7);
-
-void setup() {
-  lcd.begin(20,4);
-  lcd.setCursor(0, 0);
-  //         01234567890123456789
-  lcd.print("BTC Wallet Gen");
-  lcd.setCursor(0, 1);
-  lcd.print("B1:gen B2:next");
-}
-
-void loop() {
-  lcd.setCursor(0, 1);
-  lcd.print(pkwif);
-  lcd.print(pubkstr)
-  delay(1000);
-}
-*/
 
 #ifdef ESP8266
 #include <ESP8266TrueRandom.h>
@@ -50,11 +35,16 @@ uint8_t generate_random_8bit()
 void setup()
 {
     // setup leds
+    pinMode(button1, INPUT);
+    pinMode(button2, INPUT);
+    pinMode(led_board, OUTPUT);
     // setup buttons
+
+    lcd.begin(20, 4);
 
 #ifdef DEBUG
     // set up serial if in debug mode
-    Serial.begin(115200);
+    Serial.begin(9600);
     // wait until serial is connected.
     while (!Serial)
         ;
@@ -72,17 +62,16 @@ void setup()
 // generate private key and public address
 void generate_wallet()
 {
-    const int pklen = 256 / 8;
+    const uint8_t pklen = 32;
     uint8_t pkbytes[pklen];
 
-    for (int i = 0; i < pklen; i++)
+    for (uint8_t i = 0; i < pklen; i++)
     {
-        uint8_t rn = generate_random_8bit();
-        pkbytes[i] = rn;
+        pkbytes[i] = generate_random_8bit();
     }
 
     pkstr = "";
-    for (int i = 0; i < pklen; i++)
+    for (uint8_t i = 0; i < pklen; i++)
     {
         if (pkbytes[i] <= 0x0f)
         {
@@ -109,30 +98,87 @@ void generate_wallet()
 #endif
 }
 
+int action = 1;
+// action = 0 ==> don't update lcd
+// action = 1 ==> init show info
+// action = 2 ==> next page
+// action = 3 ==> update wallet
+int page = 1;
+int edge_detector_create_wallet = LOW, edge_detector_lcd = LOW;
+
 void loop()
 {
-    if (no_wallet_created_so_far)
+    switch (action)
     {
-        // show info on how to create a wallet
-    }
-    else
-    {
+    case 0:
+        break;
+    case 1:
+        lcd.setCursor(0, 0);
+        //         01234567890123456789
+        lcd.print("BTC Wallet Gen");
+        lcd.setCursor(0, 1);
+        lcd.print("B1:gen B2:next");
+        lcd.setCursor(0, 2);
+        lcd.print("by Felix");
+        lcd.setCursor(0, 3);
+        lcd.print("Weichselgartner");
+        break;
+    case 2:
+        if (page == 1)
+        {
+            page = 2;
+        }
+        else
+        {
+            page = 1;
+        }
+    case 3:
         // display private key (in wif): pkwif (pkstr - optional)
         // display public address for user: pubkstr
         // on lcd display
+        if (page == 1)
+        {
+
+            lcd.setCursor(0, 0);
+            //         01234567890123456789
+            lcd.print("Wallet Import Format");
+            lcd.setCursor(0, 1);
+            lcd.print(pkwif);
+        }
+        else
+        {
+            lcd.setCursor(0, 0);
+            //         01234567890123456789
+            lcd.print("Public Key");
+            lcd.setCursor(0, 1);
+            lcd.print(pubkstr);
+        }
+
+        break;
     }
+
+    action = 0;
 
     // catch buttons
 
+    int button2_state = digitalRead(button2);
     // create wallet button
-    if (false)
+    if (button2_state != edge_detector_create_wallet && button2_state == HIGH)
     {
-
         no_wallet_created_so_far = false;
+        generate_wallet();
+        action = 3;
     }
 
+    edge_detector_create_wallet = button2_state;
+
+    int button1_state = digitalRead(button1);
     // next page on lcd button
-    if (false)
+    if (button1_state != edge_detector_lcd && button1_state == HIGH)
     {
+        Serial.println("next page");
+        action = 2;
     }
+
+    edge_detector_lcd = button1_state;
 }
